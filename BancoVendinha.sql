09151f6effb82248ea0f0a3dd41cbb1e7db061bb
Maria Fernanda <uchihaunohana@gmail.com>
2026-05-30 22:36:31 -0300

    Add files via upload

---
+++ BancoVendinha.sql
@@ -0,0 +1,33 @@
+-- Database: Vendinha
+
+-- DROP DATABASE IF EXISTS "Vendinha";
+
+CREATE DATABASE "Vendinha"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+
+
+	CREATE TABLE clientes (
+    id SERIAL PRIMARY KEY,
+    nome_completo VARCHAR(200) NOT NULL,
+    cpf VARCHAR(14) NOT NULL,
+    data_nascimento TIMESTAMP NOT NULL,
+    email VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE dividas (
+    id SERIAL PRIMARY KEY,
+    valor DECIMAL(18,2) NOT NULL,
+    paga BOOLEAN NOT NULL DEFAULT FALSE,
+    data_criacao TIMESTAMP NOT NULL,
+    data_pagamento TIMESTAMP,
+    cliente_id INT NOT NULL REFERENCES clientes(id) 
+);
